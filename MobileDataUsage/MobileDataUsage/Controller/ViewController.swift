e656916f513bd73b8a1cf4e6abd57d075948dcb0
Gopalasetti, Siva <sivagopalasetti@gmail.com>
2018-11-28 21:10:46 +0530

    Initial commit. Project Created.

---
+++ MobileDataUsage/MobileDataUsage/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileDataUsage
+//
+//  Created by Gopalasetti, Siva on 28/11/18.
+//  Copyright © 2018 Honeywell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
